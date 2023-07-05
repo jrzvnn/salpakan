d86a76b633b519c4fe8014decd9f5e072d6ef887
jrzvnn <jrzvnn@gmail.com>
2023-07-06 01:44:54 +0800

    Add 9x8 board

---
+++ game_logic/piece.py
@@ -0,0 +1,10 @@
+class Piece:
+    def __init__(self, rank, player):
+        self.rank = rank
+        self.player = player
+
+    def move(self, destination):
+        pass
+
+    def capture(self, target_piece):
+        pass
--- main.py
+++ main.py
@@ -1,24 +1,72 @@
 import pygame
 import sys
 
-pygame.init()
+# Window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 
-# Set up the game window
-WIDTH = 800
-HEIGHT = 800
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Salpakan")
+# Board dimensions
+BOARD_ROWS = 8
+BOARD_COLS = 9
 
-clock = pygame.time.Clock()
+# Colors
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+BLACK = (0, 0, 0)
 
+# Cell width and height (including the stretching)
+CELL_WIDTH = (WINDOW_WIDTH - 10) // BOARD_COLS
+CELL_HEIGHT = (WINDOW_HEIGHT - 10) // BOARD_ROWS
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+def draw_board(screen):
+    screen.fill(WHITE)
 
+    # Calculate the board dimensions
+    board_width = CELL_WIDTH * BOARD_COLS
+    board_height = CELL_HEIGHT * BOARD_ROWS
 
+    # Calculate the offset to center the board
+    x_offset = (WINDOW_WIDTH - board_width) // 2
+    y_offset = (WINDOW_HEIGHT - board_height) // 2
 
-    pygame.display.flip()  # Update the contents of the entire display
-    clock.tick(60)  # Limit the frame rate to 60 FPS
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            x = x_offset + col * CELL_WIDTH
+            y = y_offset + row * CELL_HEIGHT
+
+            # Determine the cell color based on the row
+            if row < BOARD_ROWS // 2:
+                color = GRAY
+            else:
+                color = WHITE
+
+            pygame.draw.rect(screen, color, (x, y, CELL_WIDTH, CELL_HEIGHT))
+            pygame.draw.rect(screen, BLACK, (x, y, CELL_WIDTH, CELL_HEIGHT), 1)
+
+    # Draw horizontal lines
+    for row in range(BOARD_ROWS + 1):
+        y = y_offset + row * CELL_HEIGHT
+        pygame.draw.line(screen, BLACK, (x_offset, y), (x_offset + board_width, y), 1)
+
+    # Draw vertical lines
+    for col in range(BOARD_COLS + 1):
+        x = x_offset + col * CELL_WIDTH
+        pygame.draw.line(screen, BLACK, (x, y_offset), (x, y_offset + board_height), 1)
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Game of Generals")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        draw_board(screen)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+
