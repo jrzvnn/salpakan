3cc56464bd30c5d5dcc428804215fed5c429a590
jrzvnn <jrzvnn@gmail.com>
2023-07-06 02:43:19 +0800

    Add piece representations

--- game_logic/piece.py
+++ game_logic/piece.py
@@ -3,8 +3,25 @@ class Piece:
         self.rank = rank
         self.player = player
 
-    def move(self, destination):
-        pass
-
     def capture(self, target_piece):
-        pass
+        if self.rank == "Spy" and target_piece.rank == "Private":
+            return True
+        return RANKS[self.rank] > RANKS[target_piece.rank]
+
+
+RANKS =  {
+    "Five Star General": 12,
+    "Four Star General": 11,
+    "Three Star General": 10,
+    "Two Star General": 9,
+    "One Star General": 8,
+    "Colonel": 7,
+    "Lt. Colonel": 6,
+    "Major": 5,
+    "Captain": 4,
+    "1st Lieutenant": 3,
+    "2nd Lieutenant": 2,
+    "Sergeant": 1,
+    "Private": 0,
+
+}
